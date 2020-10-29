ca15994471cc5fa6f858cc1695e759da38252490
Immanuel Hardjo <31676311+KutuIjo@users.noreply.github.com>
2020-10-29 11:20:11 +0700

    Added index.js and .env file

--- index.js
+++ index.js
@@ -10,8 +10,14 @@ if (process.env.NODE_ENV !== "production") {
 
 const STORAGE_ACCOUNT_NAME = process.env.AZURE_STORAGE_ACCOUNT_NAME;
 const ACCOUNT_ACCESS_KEY = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
+const CONTAINER_NAME = process.env.AZURE_STORAGE_CONTAINER_NAME;
+const BLOB_FILEPATH = process.env.AZURE_STORAGE_BLOB_FILEPATH;
 
 const ONE_MINUTE = 60 * 1000;
+
+String.prototype.splice = function(idx, rem, str) {
+  return this.slice(0, idx) + str + this.slice(idx + Math.abs(rem));
+};
 
 async function showContainerNames(aborter, blobServiceClient) {
     let iter = await blobServiceClient.listContainers(aborter);
@@ -19,8 +25,8 @@ async function showContainerNames(aborter, blobServiceClient) {
       console.log(` - ${container.name}`);
     }
 }
-async function showBlobNames(aborter, containerClient) {
 
+async function showBlobNames(aborter, containerClient) {
     let iter = await containerClient.listBlobsFlat(aborter);
     for await (const blob of iter) {
       console.log(` - ${blob.name}`);
@@ -41,10 +47,24 @@ async function streamToString(readableStream) {
     });
 }
 
-async function execute() {
+function parseBlobContent(downloadedString) {
+  var result = "["+downloadedString;
+  var startlocation = 1;
+  var loglocation = 1;
+  while (loglocation != -1) {
+    loglocation = result.indexOf("Z\"}}", startlocation);
+    console.log(loglocation);
+    result = result.splice((loglocation + 4), 0, ",");
+    startlocation = loglocation + 5;
+  }
+  result = "[{\"" + result.slice(4, -1) + "]";
+  data = JSON.parse(result);
+  return data;   
+}
 
-    const containerName = "<containername>";
-    const blobName = "<filepathtoblob>";
+async function execute() {
+    const containerName = CONTAINER_NAME;
+    const blobName = BLOB_FILEPATH;
 
     const credentials = new StorageSharedKeyCredential(STORAGE_ACCOUNT_NAME, ACCOUNT_ACCESS_KEY);
 
@@ -64,7 +84,10 @@ async function execute() {
     const downloadResponse = await blockBlobClient.download(0,aborter);
     const downloadedContent = await streamToString(downloadResponse.readableStreamBody);
 
-    console.log(`Downloaded blob content: "${downloadedContent}"`);
+    var data = parseBlobContent(downloadedContent);
+    console.log(`Data: "${data}"`);
+
+    var statistics = calculateData(data);
 }
 
 execute().then(() => console.log("Done")).catch((e) => console.log(e));
